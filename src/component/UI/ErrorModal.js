0a1244644f752552a4fa0b9096094da7232d56e7
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-03-21 22:47:44 +1100

    working with portals

--- src/component/UI/ErrorModal.js
+++ src/component/UI/ErrorModal.js
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import ReactDOM from 'react-dom'
 import { Button } from './Button'
 import { Card } from './Card'
 import classes from './ErrorModal.module.css'
 
+const Backdrop = props => {
+    return (
+        <div className={classes.backdrop} onClick={props.errorHandler}></div>
+    )
+}
+
+const ModalOverlay = props => {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>
+                    {props.message}
+                </p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.errorHandler}>Okay</Button>
+            </footer>
+        </Card>
+    )
+}
 
 export const ErrorModal = (props) => {
     return (
-        <div>
-            <div className={classes.backdrop} onClick={props.errorHandler}></div>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>
-                        {props.message}
-                    </p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.errorHandler}>Okay</Button>
-                </footer>
-            </Card>
-        </div>
+        <Fragment>
+            {ReactDOM.createPortal(<Backdrop errorHandler={props.errorHandler} />, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<ModalOverlay
+                title={props.title}
+                message={props.message}
+                errorHandler={props.errorHandler}
+            />, document.getElementById('modal-root'))}
+        </Fragment>
     )
 }
